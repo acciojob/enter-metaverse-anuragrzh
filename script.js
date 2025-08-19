e8953fe10e6b80a48d2edfc802eabae3f2f4f01f
anuragsoni5643 <sonianurag506@gmail.com>
2025-08-19 17:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let clickBtn=document.querySelector("#enterBtn")
+let para =document.querySelector("#status")
+clickBtn.onclick= function(){
+	changeText();
+}
+
+ function changeText(){
+	para.innerHTML="<h1>Entered Metaverse</h1> "
+	
+}
